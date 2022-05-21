8f5a7d649ab8bf6ad4277d4681fb24550fe37f52
hunzsig <mzyhaohaoren@qq.com>
2022-05-21 15:11:06 +0800

    chore(*): *

--- scripts/process/battle.lua
+++ scripts/process/battle.lua
@@ -2,6 +2,20 @@ local process = Process("battle")
 
 process:onStart(function(this)
 
+    local size = 512
+    local zOffset = 0
+    local i = J.CreateImage("ReplaceableTextures\\Splats\\AuraRune9b.blp", size, size, size, 0, 0, zOffset, 0, 0, 0, 3)
+    J.SetImageAboveWater(i, true, true)
+    J.SetImageRenderAlways(i, true)
+    J.ShowImage(i, true)
+    this:stage("image", i)
+    time.setInterval(2, function()
+        async.call(Player(1), function()
+            J.SetImagePosition(i, japi.DzGetMouseTerrainX() - size / 2, japi.DzGetMouseTerrainY() - size / 2, japi.DzGetMouseTerrainZ())
+        end)
+        J.SetImageConstantHeight(i, true, japi.DzGetMouseTerrainZ())
+    end)
+
     local u1 = Unit(TPL_UNIT.HeroFlameLord, Player(1), 400, -1000, 66.6)
         :iconMap("px", 0.01, 0.01)
         :level(1)
@@ -52,6 +66,8 @@ process:onStart(function(this)
 end)
 
 process:onOver(function(this)
+    J.ShowImage(this:stage("image"), false)
+    J.DestroyImage(this:stage("image"))
     destroy(this:stage("u1"))
     local u2s = this:stage("u2s")
     for _, v in ipairs(u2s) do
