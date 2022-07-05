5d709b2d89ef898bce6a360380f7feaccb663616
hunzsig <mzyhaohaoren@qq.com>
2022-07-05 21:06:56 +0800

    chore(*): up

--- scripts/process/t2r.lua
+++ scripts/process/t2r.lua
@@ -8,11 +8,11 @@ process:onStart(function(this)
 
     local t2rFrame = FrameBackdrop("t2r", FrameGameUI)
         :relation(FRAME_ALIGN_CENTER, FrameGameUI, FRAME_ALIGN_LEFT_BOTTOM, 0, 0)
-        :size(0.08, 0.004)
+        :size(0.04, 0.004)
         :texture(TEAM_COLOR_BLP_PURPLE)
 
     japi.Refresh("t2r", function()
-        local rx, ry = japi.DzConvertWorldPosition(u1:x(), u1:y(), 150 + u1:stature() + u1:h())
+        local rx, ry = japi.DzConvertWorldPosition(u1:x(), u1:y(), 130 + u1:stature() + u1:h())
         if (rx == -1 or ry == -1) then
             t2rFrame:show(false)
             return
